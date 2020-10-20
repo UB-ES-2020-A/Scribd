1f6ea89172e54135c40cf3d458883cd4c7fb458d
marcosplazagonzalez <marcos.plaza.gonzalez@gmail.com>
2020-10-20 19:56:33 +0200

    Primera prueba

--- Scribd/models.py
+++ Scribd/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+# comentario de prueba
 class ebook(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=1024)
