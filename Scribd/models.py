1094e1333d2c9e39f12ddc0e9de47d560094a0fc
johnnync13 <johnnynuca14@gmail.com>
2020-10-20 19:47:51 +0200

    - Added json ebooks.json - New model books - Update admin.py registration

--- Scribd/models.py
+++ Scribd/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class ebook(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=1024)
+    autor = models.CharField(max_length=1024)
+    size = models.IntegerField()
+    media_type= models.CharField(max_length=16)
+    count_downloads = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Ebooks'
+        verbose_name_plural = 'Ebooks'
+
+    def __str__(self):
+        return self.title
