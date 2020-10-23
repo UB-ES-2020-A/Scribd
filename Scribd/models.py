597ad334227cc935d525b9c3758c617d68d6e6b3
esterSeguraUB <estersg98@gmail.com>
2020-10-23 18:38:24 +0200

    subscribed account

--- Scribd/models.py
+++ Scribd/models.py
@@ -4,27 +4,24 @@ from django.contrib.auth.models import User
 
 
 class Account(models.Model):
-    username = models.CharField(primary_key=True, max_length=15)
+    username = models.CharField(primary_key=True, unique=True, max_length=15)
     name = models.CharField(max_length=1000)
-    email = models.CharField(max_length=1000)
+    email = models.CharField(unique=True, max_length=1000)
     date_registration = models.DateField()
-    date_registration = models.DateField()
-    subscribed = models.BooleanField()
+    subscription = models.BooleanField()
     type = models.CharField(max_length=20)
 
-    """def get_absolute_url(self):
-        return reversed('blog_post_detail', args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post, self).save(*args,**kwargs)"""
-
     class Meta:
-
+        verbose_name = 'Account'
+        ordering = ['username']
 
         def __str__(self):
             return self.username
+
+class SubscribedAccounts(models.Model):
+    username = models.ForeignKey(Account, on_delete=models.CASCADE)
+    date_subs = models.DateField()
+    free_trial = models.BooleanField()
 
 
 
