bdb556e08e5230861baae034a9a112555c8ced71
johnnync13 <johnnynuca14@gmail.com>
2020-10-29 16:42:15 +0100

    fix deployment

--- Scribd/urls.py
+++ Scribd/urls.py
@@ -14,21 +14,12 @@ urlpatterns = [
     url('ebook/$', ebookList.as_view()),
     url('ebook/(?P<pk>[0-9]+)/$', ebookDetail.as_view()),
     url('User/$', UserList.as_view()),
+    url('User/(?P<pk>[0-9]+)/$', UserDetail.as_view()),
+    url('addbook/', ebook_create_view, name='add_book'),
+    url('accounts/login/', login_create_view, name='login'),
+    url('accounts/signup/', signup_create_view, name='signup'),
+    url('User/$', UserList.as_view()),
     url('User/(?P<pk>[0-9]+)/$', UserDetail.as_view())
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-url(r'^$', lista_libros, name='mainpage'),
-url(r'^base/$', base, name='base'),
-url('predef-ebooklist/$', ebookList.as_view(), name='ebook_list'),
-url('predef-ebookdetail/(?P<pk>[0-9]+)/$', ebookDetail.as_view(), name='ebook_detail'),
-url('ebooklist/$', ebookListView.as_view(), name='ebook_custom_list'),
-url('ebookdetail/(?P<pk>[0-9]+)/$', ebookDetailView.as_view(), name='ebook_custom_detail'),
-url('addbook/', ebook_create_view, name='add_book'),
-url('accounts/login/', login_create_view, name='login'),
-url('accounts/signup/', signup_create_view, name='signup'),
-url('User/$', UserList.as_view()),
-url('User/(?P<pk>[0-9]+)/$', UserDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
