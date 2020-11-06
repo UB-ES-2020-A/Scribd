7ba4f3b4cf1038f9407049c8385ce4c44c66a75b
johnnync13 <johnnynuca14@gmail.com>
2020-11-06 19:34:26 +0100

    Create first test to model ebook

--- Scribd/tests.py
+++ Scribd/tests.py
@@ -1,1 +1,12 @@
 # Create your tests here.
+from django.test import TestCase
+from Scribd.models import Ebook
+
+class EbookTestCase(TestCase):
+    def setUp(self):
+        Ebook.objects.create(name="Don Quijote", autor="Miguel de Cervantes", description="", is_promot=False,size=2,media_type="pdf")
+
+    def test_ebook_can_create(self):
+        quijote = Ebook.objects.get(name="Don Quijote")
+        self.assertEqual(quijote.autor, 'Miguel de Cervantes')
+        self.assertEqual(quijote.is_promot, False)
