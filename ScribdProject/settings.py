710dfb5bb06bc489a0eb161b55a684956e4a0204
johnnync13 <johnnynuca14@gmail.com>
2020-12-13 21:29:00 +0100

    update

--- ScribdProject/settings.py
+++ ScribdProject/settings.py
@@ -148,9 +148,15 @@ WSGI_APPLICATION = "ScribdProject.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'johnnynunez',
+        'USER': 'johnnynunez',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'CONN_MAX_AGE': 500
+
     }
 }
 
